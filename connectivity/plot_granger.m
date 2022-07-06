190c15f227155767b4637024e1ddad90ab4df590
nastarandarjani <68055679+nastarandarjani@users.noreply.github.com>
2022-07-06 10:41:44 +0430

    Add files via upload

--- connectivity/plot_granger.m
+++ connectivity/plot_granger.m
@@ -11,38 +11,74 @@ function plot_granger()
     fb = load('../data/result/granger/gr_-0.2_0.mat', 'fstat');
     fb = fb.fstat;
     
-    figure;
-    subplot(1, 3, 1);
-    plot_pw(squeeze(mean(f1, 1)));
-    yticklabels(chnl);
-    xticklabels(chnl);
-    xtickangle(90);
-    title('first peak');
-    caxis([0, 0.0410]);
-    colorbar;
+    % replace negative value with zero
+    f1(f1 < 0) = 0;
+    f2(f2 < 0) = 0;
+    fb(fb < 0) = 0;
     
-    subplot(1, 3, 2);
-    plot_pw(squeeze(mean(f2, 1)));
-    yticklabels(chnl);
-    xticklabels(chnl);
-    xtickangle(90);
-    title('second peak');
-    caxis([0, 0.0410]);
-    colorbar;
+    % compute p-value based on signrank
+    p1 = nan(21, 21);
+    p2 = nan(21, 21);
+    pb = nan(21, 21);
+    for i=1:21
+        for j=1:21
+            if i==j
+                continue;
+            end
+            p1(i, j) = signrank(f1(:, i, j));
+            p2(i, j) = signrank(f2(:, i, j));
+            pb(i, j) = signrank(fb(:, i, j));
+        end
+    end
+    % apply FDR correction
+    p1 = FDR_(p1);
+    p2 = FDR_(p2);
+    pb = FDR_(pb);
     
-    subplot(1, 3, 3);
-    plot_pw(squeeze(mean(fb, 1)));
-    yticklabels(chnl);
-    xticklabels(chnl);
-    xtickangle(90);
-    title('baseline');
-    caxis([0, 0.0410]);
-    colorbar;
-    
+    % average granger casaulity value
     f1 = squeeze(mean(f1, 1));
     f2 = squeeze(mean(f2, 1));
     fb = squeeze(mean(fb, 1));
     
+    % set non-significant connections to zero
+    f1 = (p1 <= 0.05) .* f1;
+    fb = (pb <= 0.05) .* fb;
+    f2 = (p2 <= 0.05) .* f2;
+    
+    figure;
+    subplot(1, 3, 2);
+    plot_pw(f1, parula);
+    yticklabels(chnl);
+    xticklabels(chnl);
+    xtickangle(90);
+    title('first peak connections');
+    caxis([0, max([f1(:); f2(:); fb(:)])]);
+    colorbar;
+    
+    subplot(1, 3, 3);
+    plot_pw(f2, parula);
+    yticklabels(chnl);
+    xticklabels(chnl);
+    xtickangle(90);
+    title('second peak connections');
+    caxis([0, max([f1(:); f2(:); fb(:)])]);
+    colorbar;
+    
+    subplot(1, 3, 1);
+    plot_pw(fb, parula);
+    yticklabels(chnl);
+    xticklabels(chnl);
+    xtickangle(90);
+    title('baseline connections');
+    caxis([0, max([f1(:); f2(:); fb(:)])]);
+    colorbar;
+    
+    topoplotFC_(f1 - fb, chnl);
+    title('normalised first peak connections')
+    topoplotFC_(f2 - fb, chnl);
+    title('normalised second peak connections')
+%     topoplotFC_(fb, chnl);
+       
     merged = [nansum(f1(1:8, :)); nansum(f1(9:12, :)); ...
         nansum(f1(13:16, :)); nansum(f1(17:end, :))];
     merged_f1 = [nansum(merged(:, 1:8), 2), nansum(merged(:, 9:12), 2),...
@@ -56,25 +92,41 @@ function plot_granger()
     merged_fb = [nansum(merged(:, 1:8), 2), nansum(merged(:, 9:12), 2),...
         nansum(merged(:, 13:16), 2), nansum(merged(:, 17:end), 2)];
     
-    figure;
-    subplot(1, 3, 1)
-    plot_pw(merged_f1)
-    colorbar
-    caxis([0, 0.2788])
-    subplot(1, 3, 2)
-    plot_pw(merged_f2)
-    colorbar
-    caxis([0, 0.2788])
-    subplot(1, 3, 3)
-    plot_pw(merged_fb)
-    colorbar
-    caxis([0, 0.2788])
+    idx = logical(eye(size(merged_f1)));
+    merged_f1(idx) = nan;
+    merged_f2(idx) = nan;
+    merged_fb(idx) = nan;
     
     figure;
+    subplot(1, 3, 2)
+    plot_pw(merged_f1, parula)
+    colorbar
+    caxis([0, max([merged_f1(:); merged_f2(:); merged_fb(:)])])
+    title('first peak region connections')
+    subplot(1, 3, 3)
+    plot_pw(merged_f2, parula)
+    colorbar
+    caxis([0, max([merged_f1(:); merged_f2(:); merged_fb(:)])])
+    title('second peak region connections')
+    subplot(1, 3, 1)
+    plot_pw(merged_fb, parula)
+    colorbar
+    caxis([0, max([merged_f1(:); merged_f2(:); merged_fb(:)])])
+    title('baseline region connections')
+        
+    norm_f1 = merged_f1-merged_fb;
+    norm_f2 = merged_f2-merged_fb;
+    figure;
     subplot(1, 2, 1)
-    plot_pw(merged_f1-merged_fb)
+    imagesc(norm_f1);
+    caxis([min([norm_f1(:); norm_f2(:)]), max([norm_f1(:); norm_f2(:)])]);
+    title('normalized first peak region connections');
+    axis('square');
     colorbar
     subplot(1, 2, 2)
-    plot_pw(merged_f2-merged_fb)
-    colorbar    
+    imagesc(norm_f2);
+    caxis([min([norm_f1(:); norm_f2(:)]), max([norm_f1(:); norm_f2(:)])]);
+    title('normalized second peak region connections');
+    axis('square');
+    colorbar
 end
